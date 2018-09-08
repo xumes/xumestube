8d2f2b65dc2cb9973d720fcbddc95c59618817b2
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-08 12:56:13 -0300

    conectei o componente App com a Store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+
 import './App.css';
 import SearchBar from './componentes/SearchBar';
+import store from './store'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store={store}>
         <SearchBar />
-      </div>
+      </Provider>
     );
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
-import {createStore} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 
 const store = createStore(
     reducers,
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
