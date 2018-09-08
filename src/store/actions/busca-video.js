17dc5c76d69f003ac6f77109cc519ef8847feab1
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-08 12:46:29 -0300

    convertendo as actions em assincronas

--- src/store/actions/busca-video.js
+++ src/store/actions/busca-video.js
@@ -1,6 +1,36 @@
-export const buscaVideo = (termo) => {
+import youtubeSearch from 'youtube-api-v3-search';
+
+const API_KEY = 'AIzaSyD01231HDFRLa9B6iwVWORGI5SRexxKXSk'
+
+export const buscaVideoRequest = () => {
     return {
-        type: 'BUSCA_VIDEO',
-        payload: { termo }
+        type: 'BUSCA_VIDEO_REQUEST',
+        carregando: true,
+        erro: false
+    }
+}
+
+export const buscaVideoSucess = (data) => {
+    return {
+        type: 'BUSCA_VIDEO_SUCCESS',
+        data,
+        carregando: false,
+        erro: false
+    }
+}
+
+export const buscaVideoError = () => {
+    return {
+        type: 'BUSCA_VIDEO_ERROR',
+        carregando: false,
+        erro: true
+    }
+}
+
+export const buscaVideo = (termo) => {
+    return dispatch => {
+        dispatch(buscaVideoRequest())
+        youtubeSearch(API_KEY, { q: termo })
+            .then((data) => console.log(data))
     }
 }
