2534f150c55df935d26feb09d5ecdb6bcd45b753
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-09 19:08:45 -0300

    Video Player com imagem estática para carregamento

--- src/componentes/VideoPlayer.js
+++ src/componentes/VideoPlayer.js
@@ -4,6 +4,7 @@ const VideoPlayer = props => {
     return (
         <div className='video-player'>
             <h2>Video Player</h2>
+            <img src='https://i.pinimg.com/originals/a2/81/38/a28138c18f0e24e9bab084aaa97770be.jpg' />
         </div>
     )
 }
