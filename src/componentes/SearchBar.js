251044a0b376e063a06094d27f93ba2ade54df83
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-09 18:14:08 -0300

    implementei o redux logger para controlar o fluxo de ações implementei a busca de videos conectei o componente SearchBar na Store

--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { buscaVideo } from './../store/actions/busca-video'
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-
-    buscaVideo = (e) => {
-        if (e.keyCode === 13){
-            console.log(e.target.value)
+    pesquisaTermo = (e) => {
+        if (e.keyCode === 13) {
+            const termo = e.target.value;
+            console.log(termo)
+            this.props.buscaVideo(termo)
         }
-        
+
     }
 
     render() {
         return (
-            <input type='text' onKeyDown={(e) => this.buscaVideo(e)} />
+            <input type='text' onKeyDown={(e) => this.pesquisaTermo(e)} />
         )
     }
 }
 
-export default SearchBar
+const mapStateToProps = (state) => {
+    return {
+        carregando: state.busca.carregando,
+        erro: state.busca.erro,
+        videos: state.busca.videos
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buscaVideo: (termo) => dispatch(buscaVideo(termo))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar)
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
 import reducers from './reducers'
 
 const store = createStore(
     reducers,
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk, logger)
 )
 
 export default store
