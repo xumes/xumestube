960a96bf41793b2bc36ae18f641237a224a7bdb8
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-18 22:04:06 -0300

    implementando estilo ao vide player

--- src/componentes/VideoPlayer.js
+++ src/componentes/VideoPlayer.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Embed } from 'semantic-ui-react'
 
 const VideoPlayer = props => {
     return (
         <div className='video-player'>
             <h2>Video Player</h2>
-            <img src='https://i.pinimg.com/originals/a2/81/38/a28138c18f0e24e9bab084aaa97770be.jpg' />
+            {
+                props.video && props.video.snippet && (
+                    <div>
+                    <Embed id={props.video.id.videoId} placeholder='https://i.pinimg.com/originals/a2/81/38/a28138c18f0e24e9bab084aaa97770be.jpg' source='youtube' />
+                        <p>{props.video.snippet.title}</p>
+                        <p>{props.video.snippet.description}</p>
+                    </div>
+                )
+            }
         </div>
     )
 }
 
-export default VideoPlayer
+const mapStateToProps = (state) => {
+    return {
+        video: state.reproduz.video
+    }
+}
+
+export default connect(mapStateToProps, null)(VideoPlayer)
