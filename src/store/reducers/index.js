5c6d39d30af886255c79e76290c5b930cfd2bba6
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-18 22:04:33 -0300

    criada reducer reproduz video

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,12 @@
 import {combineReducers} from 'redux'
 
 import busca from './busca'
+import reproduz from './reproduz-video'
 
 const rootReducer = combineReducers({
     busca,
+    reproduz
+
 })
 
 export default rootReducer
