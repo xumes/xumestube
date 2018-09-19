6da32d6ef00128c3a4e95dfae59243cba75b3781
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-18 22:04:40 -0300

    criada reducer reproduz video

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
 import reducers from './reducers'
 
-const store = createStore(
-    reducers,
-    applyMiddleware(thunk, logger)
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = [thunk, logger]
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(...middlewares)
+));
 
 export default store
