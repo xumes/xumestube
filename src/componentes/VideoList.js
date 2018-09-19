190a75b6deb301a8cadd0a29f6764047f1909e35
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-18 22:03:55 -0300

    implementando estilo a video list

--- src/componentes/VideoList.js
+++ src/componentes/VideoList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { List, Image, Dimmer, Loader } from 'semantic-ui-react'
 
+import { reproduzVideo } from '../store/actions/reproduz-video';
 
 class VideoList extends Component {
     constructor(props) {
@@ -9,29 +11,46 @@ class VideoList extends Component {
         this.renderVideo = this.renderVideo.bind(this)
     }
 
+    carregaVideo(video) {
+
+    }
+
     renderVideo(video) {
         return (
-            <li key={video.etag}>
-                <p>{video.snippet.title}</p>
-                <img src={video.snippet.thumbnails.default.url} />
-                <p>{video.snippet.description}</p>
-            </li>
+            <List animated verticalAlign='middle'>
+                <List.Item onClick={() => this.props.reproduz(video)}>
+                    <Image avatar src={video.snippet.thumbnails.default.url} />
+                    <List.Content>
+                        <List.Header>{video.snippet.title}</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
         )
     }
 
     render() {
         return (
+
             <div className='video-list'><h2>Lista de Videos </h2>
-                <ul>
-                    {
-                        this.props.videos.map(video => {
-                            console.log("olha eu aqui", video)
-                            return this.renderVideo(video)
-                        })
-                    }
-                </ul>
+                {
+                    this.props.carregando && (<Dimmer active inverted>
+                        <Loader size='medium'>Loading</Loader>
+                    </Dimmer>)
+                }
+                {
+                    this.props.videos.map(video => {
+                        console.log("olha eu aqui", video)
+                        return this.renderVideo(video)
+                    })
+                }
             </div>
         )
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        reproduz: (video) => dispatch(reproduzVideo(video))
     }
 }
 
@@ -39,8 +58,9 @@ const mapStateToProps = (state) => {
     return {
         carregando: state.busca.carregando,
         erro: state.busca.erro,
-        videos: state.busca.videos
+        videos: state.busca.videos,
+        video: state.reproduz.video
     }
 }
 
-export default connect(mapStateToProps, null)(VideoList)
+export default connect(mapStateToProps, mapDispatchToProps)(VideoList)
