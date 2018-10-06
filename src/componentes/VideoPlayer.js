1f5dd5e262ca3a0e18633eceffdba3c5a2eb7cfe
Reginaldo Santos <rmsantos@br.ibm.com>
2018-10-06 18:00:49 -0300

    melhorando leiaute

--- src/componentes/VideoPlayer.js
+++ src/componentes/VideoPlayer.js
@@ -7,14 +7,18 @@ const VideoPlayer = props => {
         <div className='video-player'>
         {
         
-        !props.video.id && <Advertisement unit='top banner' test='Escolha um video para reproduzir' />
+        !props.video.id && (
+            <div >
+            <Advertisement style={{'height': '433px'}} unit='top banner' test='Escolha um video para reproduzir' />
+            </div>
+        )
         }
             {
                 props.video && props.video.snippet && (
                     <div>
                     <Embed id={props.video.id.videoId} placeholder='https://i.pinimg.com/originals/a2/81/38/a28138c18f0e24e9bab084aaa97770be.jpg' source='youtube' />
                         <p className='video-titulo'>{props.video.snippet.title}</p>
-                        <p>{props.video.snippet.description}</p>
+                        <p className='video-descricao'>{props.video.snippet.description}</p>
                     </div>
                 )
             }
